0ec685ebf4e58a2430e348caa3ddefabe09a4f05
Moustafa Mahmoud <moustafafci25@gmail.com>
2021-01-23 21:57:32 +0200

    Feat: Add header

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,5 @@
 const hamburgerBtn = document.querySelector("#hamburgerBtn");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElements = document.querySelectorAll(".has-fade");
@@ -7,12 +8,14 @@ hamburgerBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if (header.classList.contains("open")) {
     header.classList.remove("open");
+    body.classList.remove("noscroll");
     fadeElements.forEach((element) => {
       element.classList.remove("fade-in");
       element.classList.add("fade-out");
     });
   } else {
     header.classList.add("open");
+    body.classList.add("noscroll");
     fadeElements.forEach((element) => {
       element.classList.remove("fade-out");
       element.classList.add("fade-in");
